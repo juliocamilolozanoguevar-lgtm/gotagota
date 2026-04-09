468fb180dea14964f354ccbfa54c01e8984a1dea
Gabriel-2415 <1680465@SENATI.PE>
2026-04-09 16:59:12 -0500

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,7 +2,10 @@
 document.addEventListener("DOMContentLoaded", () => {
    fetch('http://localhost:8080/api/clientes')
    .then(response => response.json())
-   .then(
-    data => console.log(data)
-) ;
+   .then(data => {
+    console.log('DATOS',data);
+    const elemento = document.getElementById("tabla-cliente")
+    console.log(elemento)
+      
+}) ;
 });
